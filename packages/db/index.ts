11250f19ac128ae4bfcc0595506924708a12a72b
mohd kaif <mk2818356@gmail.com>
2026-04-13 21:03:12 +0530

    fixes errors

--- packages/db/index.ts
+++ packages/db/index.ts
@@ -1,1 +1,21 @@
-console.log("Hello via Bun!");
+import { prisma } from "./client";
+export * from "./generated/prisma/client";
+export { prisma } from "./client"
+// async function main(){
+//   const res = await prisma.user.create({
+//     data : {
+//       email : "nobitaka@gmail.com",
+//       password : "password",
+//       name : "nobitakaif",
+//       image : "https://google.com"
+//     }
+//   })
+//   console.log(res.id)
+//   return res.id
+// }
+
+// main().then(r => {
+//   console.log(r)
+// }).catch(e =>{
+//   console.log(e)
+// })
